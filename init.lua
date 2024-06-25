8c7eaea6ff93ae80006e5e0caaf52587b5843454
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2024-06-25 11:44:12 +0200

    config: disable copilot by default

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-# Source legacy vimrc configuration
+-- vim: tabstop=2 shiftwidth=2 expandtab
+
+-- Source legacy vimrc configuration
 local vimrc = vim.fn.stdpath("config") .. "/legacy_vimrc.vim"
 vim.cmd.source(vimrc)
 
@@ -6,3 +8,8 @@ require("CopilotChat").setup {
   debug = true, -- Enable debugging
   -- See Configuration section for rest
 }
+-- Disable copilot by default
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Disable Copilot by default on startup",
+  command = "Copilot disable",
+})
