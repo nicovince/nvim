b0427b55f6c9aed64de9f35288327ad42c91ad3a
Nicolas VINCENT <nico.vince@gmail.com>
2025-03-15 13:09:14 +0100

    lua/ft: set lua filetype file and reindent lua files.

--- lua/nvim-lint-usercfg.lua
+++ lua/nvim-lint-usercfg.lua
@@ -1,8 +1,9 @@
 -- configuration of nvim-lint plugin
 require('lint').linters_by_ft = {
-        ghaction = { 'actionlint' },
-        python = { 'flake8' },
+  ghaction = { 'actionlint' },
+  python = { 'flake8' },
 }
 
 -- Run linter
+-- LInter Run
 vim.keymap.set('n', '<Leader>lir', function() require('lint').try_lint() end)
